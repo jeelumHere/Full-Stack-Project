d53e28b0972ab98acd5d672f452f39779ecab213
jeelumHere <syedsharjeelahmed214@gmail.com>
2026-08-15 18:47:19 +0500

    Setting Base for responsive setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      I am <React></React>
+    <div className='text-textMuted bg-primary'>
+      I am ReactJS
     </div>
   )
 }
